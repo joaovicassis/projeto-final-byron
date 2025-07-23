bcc71d5234dc6e4840212ba2088e4ff5f9625d83
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-23 13:47:07 -0300

    feat:estrutura do footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${inter.variable}  antialiased`}
       >
         <Header/>
+        <Footer/>
         {children}
       </body>
     </html>
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer>
+            <div>
+                <p className="text-white">© 2025 BiblioTech. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    )
+}
