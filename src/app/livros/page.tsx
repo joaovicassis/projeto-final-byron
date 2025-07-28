386dc8908e880185aadbdf5e02d25128d27fc320
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-28 17:55:52 -0300

    cards da secao todos

--- src/app/livros/page.tsx
+++ src/app/livros/page.tsx
@@ -1,10 +1,15 @@
+import BookCard from "@/src/components/BookCard";
+import { books } from "@/src/app/lib/livros";
+
 export default function LivrosPage(){
     return(
         <main className="flex-grow py-8">
             <div className="container mx-auto">
-                <h1 className="text-3xl font-bold">Todos os livros</h1>
-                <div>
-                    {/* livros */}
+                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+                <div className="grid grid-cols-3 gap-8 mt-8">
+                    {books.map((books)=> (
+                        <BookCard/>
+                    ))}
                 </div>
             </div>
         </main>
