997409a17d5965a1b07d3095919448c4b1eb57e0
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-28 11:19:06 -0300

    feat: estrutura da pagina todos os livros

---
+++ src/app/livros/page.tsx
@@ -0,0 +1,12 @@
+export default function LivrosPage(){
+    return(
+        <main className="flex-grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todos os livros</h1>
+                <div>
+                    {/* livros */}
+                </div>
+            </div>
+        </main>
+    )
+}
