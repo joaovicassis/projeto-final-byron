2d80f087d3f87175071593dca8ba79e2a8cc97e2
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-29 17:12:04 -0300

    fix: ajustes no design dos cards, na cor de fundo da página e insercao de mais livros a db

--- src/app/livros/page.tsx
+++ src/app/livros/page.tsx
@@ -3,7 +3,7 @@ import { books } from "@/src/app/lib/livros";
 
 export default function LivrosPage(){
     return(
-        <main className="flex-grow py-8">
+        <main className="flex-grow py-8 bg-green-50">
             <div className="container mx-auto">
                 <h1 className="text-3xl font-bold">Acervo</h1>
                 <div className="grid grid-cols-3 gap-8 mt-8">
